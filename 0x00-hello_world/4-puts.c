dfdb4151c17356f2f54460990775ba8502402e68
bmokase3093 <bmokase3093@gmail.com>
2022-03-10 15:11:55 -0800

    C Program to print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/* Program to print a string using puts function
+ * return 0
+ */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
