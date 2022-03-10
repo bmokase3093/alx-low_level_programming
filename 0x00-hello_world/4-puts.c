daa7155bd51ae21e4ed268dcae89d9b3fa97b574
bmokase3093 <bmokase3093@gmail.com>
2022-03-10 15:16:42 -0800

    C Program to print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-/* Program to print a string using puts function
+/*
+ * Program to print a string using puts function
  * return 0
  */
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
