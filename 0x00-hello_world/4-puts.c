0fec963a864a9ef73dc7f848b246efdb210c3eef
bmokase3093 <bmokase3093@gmail.com>
2022-03-10 15:36:00 -0800

    C Program to print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - starting point
  * @void: no arguments
  * Description: Program to print a string using puts fuction
- * return: value 0 to exit main fucntion
+ * Return: value 0 to exit main fucntion
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
