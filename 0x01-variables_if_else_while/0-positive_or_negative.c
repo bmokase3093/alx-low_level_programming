21c07c073656aa1b0790c34d5b8d023377cf3d1d
bmokase3093 <bmokase3093@gmail.com>
2022-03-11 08:01:41 -0800

    C Program to generate random number and print if the number is postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,18 @@
 #include<stdlib.h>
 /**
  * main - starting point
- * Description: Program to assign a random number and print if its postive or negative
+ * Description: Program to assign a random number
  * Return: value 0 on success
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n<0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	
 	}
 	else if (n > 0)
 	{
