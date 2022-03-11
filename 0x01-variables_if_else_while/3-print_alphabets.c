a43feca6493b81870888d2d6ec0193b04876e305
bmokase3093 <bmokase3093@gmail.com>
2022-03-11 14:58:58 -0800

    Print alphabtes in lowercase then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - starting point
+ *@void: no arguments
+ *Description: Print alphabet in lowercas then uppercase
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int firstSLetter, firstCletter, n;
+	
+	for (firstSLetter = 97, n = 1; n < 27; firstSLetter++, n++)
+	{
+	putchar(firstSLetter);
+	}
+	for (firstCletter = 65, n = 1; n < 27; firstCletter++, n++)
+	{
+		putchar(firstCletter);
+	}
+	putchar(10);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Program to assign random number to variable n each time it execute
 Program assign a random number. Print the last digit of the number stored in variable n
 Print alphabet in lowercas
+Print alphabets in uppercase and then lowercase
