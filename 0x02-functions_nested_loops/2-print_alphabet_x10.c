fceda76b21fe7ff7e04fbd9afb09e5ec3f62851a
bmokase3093 <bmokase3093@gmail.com>
2022-03-15 06:15:59 -0700

    Print alphabets x10 in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,8 @@
  */
 void print_alphabet_x10(void)
 {
-        char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i, j;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
 	i = 0;
 
 	while (i < 10)
