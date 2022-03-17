1180d019695f4cb7a98cf995c078821dc13aa175
bmokase3093 <bmokase3093@gmail.com>
2022-03-17 12:33:39 -0700

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,21 @@
 #include "main.h"
-#include <limits.h>
-
 /**
- * print_number - print an integer, without using long, arrays, or pointers
- * @n: number to be printed
- * Return: void
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
  */
-
 void print_number(int n)
 {
-	unsigned int tens, digit;
-	int number = n;
-	double total_tens = 1;
+	unsigned int x;
 
-	if (n == 0)
-		_putchar('0');
-	else
+	x = n;
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			number = n * -1;
-			_putchar('-');
-		}
-		while (total_tens <= number)
-			total_tens *= 10;
-		tens = total_tens / 10;
-		while (tens >= 1)
-		{
-			digit = number / tens;
-			_putchar(digit + '0');
-			number = (number - (tens * digit));
-			tens /= 10;
-		}
+		_putchar(45);
+		x = -n;
 	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
