0a665219f6fcea1235e8ce19dddca2470f865ee6
bmokase3093 <bmokase3093@gmail.com>
2022-03-22 14:42:34 -0700

    Function to return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return the length of string
+ * @s: character pointer
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int slength;
+
+	slength = strlen(*s);
+
+	return (slength);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 Function that takes a pointer to an int as parameter and updates the value it points to 98
 Swap the values of two integers
+Function that returns the length of string
