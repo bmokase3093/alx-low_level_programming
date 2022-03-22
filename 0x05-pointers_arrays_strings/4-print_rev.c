d5d9428a3c9e79d9021d7bb2f6a4e416ae5c9fe2
bmokase3093 <bmokase3093@gmail.com>
2022-03-22 15:29:17 -0700

    Function to print a string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: pointer as input
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp
+	}
+	_putchar(temp);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ Function that takes a pointer to an int as parameter and updates the value it po
 Swap the values of two integers
 Function that returns the length of string
 Print a string followed by new line
+Print a string in reverse followed by new line
