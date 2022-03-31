781d292217eea0d4b719f8dcc66377f468c48924
bmokase3093 <bmokase3093@gmail.com>
2022-03-30 17:10:53 -0700

    Function to return the value of x raised to power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - Function to return the value of x raised to pow of y
+ * @x: integer number
+ * @y: integer power
+ *
+ * Return: if y is lower than 0 return -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 Functions that printsa string followed by new line
 Returns the length of string
 Function that returns the factorial of a given number
+Function to return the value of x raised to power of y
