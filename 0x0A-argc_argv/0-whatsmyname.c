07a7513faec4e26a500ac63ee96b241c99447be0
bmokase3093 <bmokase3093@gmail.com>
2022-04-01 04:31:57 -0700

    Print the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - print the name of this program
- * @argc: count to count number of arguments passed in cmd
- * @argv: array of pointer to strings
- *
- * Return: 0 on success
- */
+  *main - prints the  name of this program.
+  *@argc: counts number of arguemnts paased in th command line.
+  *@argv: stores arguments passed in the command line.
+  *
+  *Return: 0 on success.
+  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
