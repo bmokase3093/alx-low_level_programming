b153b87b34fe3c2c33b2eb2c64ac77f46c86fbb7
bmokase3093 <bmokase3093@gmail.com>
2022-04-01 04:25:40 -0700

    Print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - print the name of this program
+ * @argc: count to count number of arguments passed in cmd
+ * @argv: array of pointer to strings
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Program to print its name followed by a new line
