e772f11502c2df80ab7409fbfa9801a2724c58e6
bmokase3093 <bmokase3093@gmail.com>
2022-04-11 18:14:25 -0700

    Define the new type struct dog with member elements

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Define the new type struct dog with member elements
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of a dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: User-defined dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
