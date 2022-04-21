4d1605c52eee828e49e304ab6892e1c199327318
bmokase3093 <bmokase3093@gmail.com>
2022-04-21 15:43:46 -0700

    Function that adds a new node at the biginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
- * _strlen - find the length of the string
- * @str: string
- *
- * Return: length of the string
- */
+  *_strlen - finds length of a string.
+  *@str: string.
+  *
+  *Return: lengt of string.
+  */
 int _strlen(const char *str)
 {
 	int i;
+
 	while (str[i] != '\0')
 		i++;
 
@@ -15,12 +16,12 @@ int _strlen(const char *str)
 }
 
 /**
- * add_node - adds a new node at the beginning of the list_t list.
- * @head: pointer to the first element of the list
- * @str: string to be duplicated
- *
- * Return: address of the new element or NULL if it failed.
- */
+  *add_node - adds a new node at the beginning of a list_t.
+  *@head: pointer to first element of the list.
+  *@str: string to be duplicated.
+  *
+  *Return: address of the new element or NULL if it failed.
+  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
@@ -28,8 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
-
-	/* temp now points to the first element*/
+	/* temp now points to first element*/
 	temp->next = *head;
 	temp->str = strdup(str);
 	temp->len = _strlen(str);
@@ -37,4 +37,5 @@ list_t *add_node(list_t **head, const char *str)
 	*head = temp;
 
 	return (*head);
+
 }
